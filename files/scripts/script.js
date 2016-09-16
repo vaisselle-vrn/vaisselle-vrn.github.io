a8155d1149c7dc204f6828432beacb57bc536d0c
Vaisselle <vaisselle.vrn@gmail.com>
2016-09-16 21:48:10 +0300

    11

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -2,11 +2,11 @@ var domain = "https://vaisselle-vrn.github.io/";
 var path = domain + "files/blocks/";
 
 function setFlags() {
-    const countries = document.getElementsByClassName("card-country");
+    var countries = document.getElementsByClassName("card-country");
     for (var i = 0; i != countries.length; i++) {
-        const country = countries[i];
-        const name = country.dataset.country.toLowerCase();
-        const flag = domain + "files/img/flags/" + name + ".png";
+        var country = countries[i];
+        var name = country.dataset.country.toLowerCase();
+        var flag = domain + "files/img/flags/" + name + ".png";
         countries[i].style.backgroundImage = "url(" + flag + ")";
     }
 }
